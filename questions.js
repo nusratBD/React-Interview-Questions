73e466f1a276be5a0360a6ce4500d23ac375d8e1
nusratBD <nusrat.hurain15@gmail.com>
2023-04-24 19:34:14 +0600

    React Interview Questions

---
+++ questions.js
@@ -0,0 +1,9 @@
+/* 
+1. How is react routing different from convetional routing?
+2. How do you implement react routing?
+3. What is the purpose of context API in react?
+4. Single Page Application vs Multi Page Application
+5. Can you explain how the Provider wokrs with the React Context API? 
+6. What is a Custom Hook?
+7. How many ways we can implement Conditional Rendering in React? Explain the ways.
+*/
